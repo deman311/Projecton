53fab8ed120392127b41791b9faed4db8f1b2e19
vv01fy <deman311@gmail.com>
2022-03-30 18:12:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.ML.OnnxRuntime;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Inferense inject the model so that it loads into memory
+builder.Services.AddSingleton<InferenceSession>(
+    new InferenceSession("./catdognone.onnx")
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
